ba4f4187e302979e7066e7094883bd5e006aee60
Venchislav <97972996+Venchislav@users.noreply.github.com>
2024-05-04 16:27:36 +0300

    Logistic Regression losses added
    Crossentropy was added to Logistic Regression list

--- Main/configurations/cfg.py
+++ Main/configurations/cfg.py
@@ -1,5 +1,6 @@
 # supported loss dictionary:
 
 supported_loss = {
-    'linear_reg': ['mae', 'mean_absolute_error', 'mean_abs_err', 'mse', 'mean_squared_error', 'mean_sq_err']
+    'linear_reg': ['mae', 'mean_absolute_error', 'mean_abs_err', 'mse', 'mean_squared_error', 'mean_sq_err'],
+    'logistic_reg': ['crossentropy', 'cross_entropy', 'binary_crossentropy']
 }
