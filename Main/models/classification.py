b1f9406282736f2c801dc5593450a566d3cf0773
Venchislav <97972996+Venchislav@users.noreply.github.com>
2024-05-04 19:00:50 +0300

    Models Update. Details lower:
    Linear Regression and Logistic Regression V 0.002
    Supports loss output

--- Main/models/classification.py
+++ Main/models/classification.py
@@ -1,4 +1,5 @@
-from Main.configurations.cfg import supported_loss
+import Main.configurations.cfg as cfg
+
 from Main.tools.validator import validate_input
 from Main.tools.funcs import sigmoid
 
@@ -11,16 +12,19 @@ class LogisticRegression:
         self.bias = None
 
         self.loss_history = []
-        self.sup_loss = supported_loss['logistic_reg']
+        self.sup_loss = cfg.supported_loss['logistic_reg']
 
     def fit(self, X, y, epochs=100, lr=0.001, loss='binary_crossentropy'):
         n_samples, n_features = X.shape
+
+        validate_input(X, y, epochs, lr, loss, self.sup_loss)  # check if everything is ok
+
         self.weights = np.zeros(n_features)
         self.bias = 0
 
         for epoch in range(epochs):
             predictions = sigmoid(np.dot(X, self.weights) + self.bias)
-
+            print(f'On Epoch {epoch} loss is {cfg.losses_addr[loss](y, predictions)}')
             dw = (1 / n_samples) * np.dot(X.T, (predictions - y))
             db = (1 / n_samples) * np.sum(predictions - y)
 
@@ -29,6 +33,3 @@ class LogisticRegression:
 
     def predict(self, X):
         return sigmoid(np.dot(X, self.weights) + self.bias)
-
-
-logi_model = LogisticRegression()
--- Main/models/regression.py
+++ Main/models/regression.py
@@ -3,8 +3,10 @@
 # and is not as customizable as scikit-learn or something like this
 # Code is under MIT license meaning it's free to share, copy, and use
 
-from Main.configurations.cfg import supported_loss
+import Main.configurations.cfg as cfg
 from Main.tools.validator import validate_input
+from Main.components.loss import mean_squared_error
+
 import numpy as np
 
 
@@ -14,17 +16,18 @@ class LinearRegression:
         self.bias = None
 
         self.loss_history = []
-        self.sup_loss = supported_loss['linear_reg']
+        self.sup_loss = cfg.supported_loss['linear_reg']
 
-    def fit(self, X, y, epochs=100, lr=0.001):
+    def fit(self, X, y, epochs=100, lr=0.001, loss='mse'):
         n_samples, n_features = X.shape
-        validate_input(X, y, epochs, lr)  # check if everything is ok
+        validate_input(X, y, epochs, lr, loss, self.sup_loss)  # check if everything is ok
 
         self.weights = np.zeros(n_features)
         self.bias = 0
 
         for epoch in range(epochs):
             predictions = self.predict(X)
+            print(f'On Epoch {epoch} loss is {cfg.losses_addr[loss](y, predictions)}')
 
             dw = (1 / n_samples) * np.dot(X.T, (predictions - y))
             db = (1 / n_samples) * np.sum(predictions - y)
@@ -33,7 +36,19 @@ class LinearRegression:
             self.bias -= 1 / n_samples * db * lr
 
 
-
     def predict(self, X):
         return np.dot(X, self.weights) + self.bias
 
+
+"""
+linear_model = LinearRegression()
+X = np.array([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+])
+y = np.array([6, 15, 24])
+
+linear_model.fit(X, y, epochs=1000)
+print(linear_model.predict(X))
+"""
