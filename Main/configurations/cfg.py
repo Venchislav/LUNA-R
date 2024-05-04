85b4f89c83741b55efa1b358bb1e2178029cc957
Venchislav <97972996+Venchislav@users.noreply.github.com>
2024-05-04 19:01:52 +0300

    Configuration file update.
    Loss functions names connected with their addresses

--- Main/configurations/cfg.py
+++ Main/configurations/cfg.py
@@ -1,6 +1,14 @@
 # supported loss dictionary:
+import Main.components.loss
 
 supported_loss = {
-    'linear_reg': ['mae', 'mean_absolute_error', 'mean_abs_err', 'mse', 'mean_squared_error', 'mean_sq_err'],
-    'logistic_reg': ['crossentropy', 'cross_entropy', 'binary_crossentropy']
+    'linear_reg': ['mae', 'mse'],
+    'logistic_reg': ['binary_crossentropy']
+}
+
+
+losses_addr = {
+    'mae': Main.components.loss.mean_absolute_error,
+    'mse': Main.components.loss.mean_squared_error,
+    'binary_crossentropy': Main.components.loss.binary_crossentropy
 }
