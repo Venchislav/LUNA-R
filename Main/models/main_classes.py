1ffb19a5fac90cfb75bfa2145a542938cadc70df
Venchislav <97972996+Venchislav@users.noreply.github.com>
2024-05-09 16:16:31 +0300

    Decision Tree will be here!

--- Main/models/main_classes.py
+++ Main/models/main_classes.py
@@ -14,3 +14,16 @@ class Model:
 
     def evaluate(self, X, y):
         return self.loss_fn(self.predict(X), y)  # don't worry. It works in subclasses
+
+
+class Node:
+    def __init__(self, feature=None, threshold=None, left=None, right=None, *, value=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = value
+
+    def is_leaf_node(self):
+        return self.value is not None
+
